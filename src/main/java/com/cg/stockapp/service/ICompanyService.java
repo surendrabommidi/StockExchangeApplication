cb77660bce691ee40a667260dbd61f7605af0c3f
Surendra babu Bommidi <surendrababu1999@gmail.com>
2021-04-11 20:55:02 +0530

    Update ICompanyService.java

--- src/main/java/com/cg/stockapp/service/ICompanyService.java
+++ src/main/java/com/cg/stockapp/service/ICompanyService.java
@@ -5,5 +5,9 @@ import com.cg.stockapp.entities.Company;
 public interface ICompanyService {
 	
 	public boolean addCompany(Company company);
+        public List<Company> getAllCompanyInfo();
+	public Company getCompanyDetails(String companyId);
+	public boolean  updateCompanyInfo(Company info);
+	public boolean deleteCompanyInfo(String companyId);
 	
 }
