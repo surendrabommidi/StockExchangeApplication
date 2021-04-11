ee52ed7129e60bdb877932de8b308dddfa47843e
Surendra babu Bommidi <surendrababu1999@gmail.com>
2021-04-11 21:14:53 +0530

    Update CompanyController.java

--- src/main/java/com/cg/stockapp/controllers/CompanyController.java
+++ src/main/java/com/cg/stockapp/controllers/CompanyController.java
@@ -21,5 +21,42 @@ public class CompanyController {
 		serv.addCompany(company);
 		return "Company added successfully";
 	}
+	
+		public List<Company> getAllCompanyInfo() {
+		List<Company> list = serv.getAllCompanyInfo();
+		if (list.size() == 0)
+			throw new EmptyTableException("company information not found in database");
+		return list;
+	}
+
+	@GetMapping(value = "{CompanyDetails}")
+	public ResponseEntity<?> getCompanyDetails(@PathVariable("CompanyDetails") String companyId) {
+		Company c = serv.getCompanyDetails(companyId);
+		if (c == null)
+
+			throw new CompanyNotFoundException("Request", "Company with id " + companyId + "Not found");
+		else
+			return new ResponseEntity<Company>(c, HttpStatus.OK);
+
+	}
+
+	@PutMapping
+	public String updateCompany(@RequestBody Company CompanyInfo) {
+		if (serv.updateCompanyInfo(CompanyInfo))
+			return "company data sucessfully updated";
+		else
+			throw new CompanyNotFoundException("update",
+					"company with Id" + CompanyInfo.getCompanyId() + "to update not found");
+
+	}
+	
+	@DeleteMapping("{eid}")
+	public String deleteCompany(@PathVariable("eid") String CompanyInfo) {
+		if(serv.deleteCompanyInfo(CompanyInfo))
+			return "Company data sucessfully deleted";
+		else
+			throw new CompanyNotFoundException("delete","Company with Id" + CompanyInfo+"Delete not found");
+	}
+
 
 }
